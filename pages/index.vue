218e5f7fb74446f45e4ec1bae7c2bd1eb37c5898
LifeSignals <venu@lifesignals.com>
2022-04-18 18:45:36 +0530

    init

---
+++ pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="container">
+        <div>
+            <Logo />
+            <Toast />
+            
+            <form @submit.prevent="greet">
+                <InputText type="text" v-model="text"/>
+                <Button type="submit" label="Submit"/>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+        text: null
+        }
+    },
+    methods: {
+        greet() {
+            this.$toast.add({severity: 'info', summary: 'Hello '  + this.text});
+        }
+    }
+}
+</script>
+
+<style>
+.container {
+    margin: 0 auto;
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
